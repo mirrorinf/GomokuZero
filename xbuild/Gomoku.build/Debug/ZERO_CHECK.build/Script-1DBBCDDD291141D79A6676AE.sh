90cce9f44607e9a9c04c8d0d3b4a989be2a400f1
mirrorinf <mirrorinf@icloud.com>
2019-12-12 20:19:11 +0800

    小事情小事情

--- xbuild/Gomoku.build/Debug/ZERO_CHECK.build/Script-1DBBCDDD291141D79A6676AE.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/chuxiaomin/Develop/Gomoku/GomokuZero/xbuild -f /Users/chuxiaomin/Develop/Gomoku/GomokuZero/xbuild/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION OBJDIR=$(basename "$OBJECT_FILE_DIR_normal") all
